237a9221fbc4cfb521eada99df7e8184289c2900
LeeVays <kamil.ulinowicz@gmail.com>
2024-12-18 02:26:14 +0100

    Praca domowa dzień 4

--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic.FileIO;
+
+var name ="Kamil";
+var sex = "Male";
+var age = 25;
+
+if (sex=="women" && age<30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+} else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+} else if (sex == "Male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (sex == "Male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else 
+{
+
+};
+
+
